681c96b6f1cf6d30f4ffabc51047e55a1e38cf9c
ZAIN AHMAD TAUFIK <62564112+ZainAhmadTaufik@users.noreply.github.com>
2020-03-24 10:03:00 +0700

    class kalkulator

---
+++ kalkulator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class kalkulator{
+  public :
+      int pertambahan();
+      int pembagian();
+      int pengurangan();
+      
+  privat :
+      float a,b,hasil;
+      }
