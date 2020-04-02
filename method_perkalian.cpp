8ed7ee073c40c15cf1b03e2fc458043ddb741239
donigagahhh <62916145+donigagahhh@users.noreply.github.com>
2020-04-02 19:42:59 +0700

    Create method_perkalian.cpp

---
+++ method_perkalian.cpp
@@ -0,0 +1,10 @@
+void perkalian(){
+	int a, b, hasil;
+	cout<<"perkalian"<<endl;
+	cout<<"masukan nilai a : ";
+	cin>>a;
+	cout<<"masukan nilai b : ";
+	cin>>b;
+	hasil = a * b;
+	cout<<"hasilnya adalah "<<hasil<<endl;
+}
